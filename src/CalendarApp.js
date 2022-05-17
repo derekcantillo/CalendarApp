8833de004c240dddf850443198d14c6c2d9d0346
unknown <cantilloderek@gmail.com>
2022-05-16 22:02:56 -0500

    first commit

---
+++ src/CalendarApp.js
@@ -0,0 +1,11 @@
+
+
+function CalendarApp() {
+  return (
+    <>
+      <h1>CalendarApp</h1>
+    </>
+  );
+}
+
+export default CalendarApp;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CalendarApp from './CalendarApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <CalendarApp />
+  </React.StrictMode>
+);
+
